7a81737f5650c1a835ce85f03d13c831164f4023
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-13 21:13:54 +0200

    Update seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -2,14 +2,54 @@ import { Boat_type, Competition, Team, db } from 'astro:db';
 
 export default async function run() {
   await db.insert(Team).values([
-    { email: 'rembadalona@gmail.com', password: 'test', name: 'Club de Rem Badalona', logo: 'default.png' },
-    { email: 'nataciobadalona@gmail.com', password: 'test', name: 'Club Natació Badalona', logo: 'default.png' },
-    { email: 'betulo@gmail.com', password: 'test', name: 'Club Nàutic Bétulo', logo: 'default.png' },
-    { email: 'aexinoxano@gmail.com', password: 'test', name: 'A.E. Xino Xano Deltebre', logo: 'default.png' },
-    { email: 'nauticflix@gmail.com', password: 'test', name: 'Club Nàutic Flix', logo: 'default.png' },
-    { email: 'vecambrills@gmail.com', password: 'test', name: "Vent d'Estrop Vogadors de Cambrills", logo: 'default.png' },
-    { email: 'remcambrills@gmail.com', password: 'test', name: 'Club de rem Cambrills', logo: 'default.png' },
-    { email: 'remmataro@gmail.com', password: 'test', name: 'Club de rem Mataró', logo: 'default.png' },
+    { 
+      email: 'rembadalona@gmail.com', 
+      password: 'test', 
+      name: 'Club de Rem Badalona', 
+      logo: 'img/logo/rembadalona.jpg' 
+  },
+  { 
+      email: 'nataciobadalona@gmail.com', 
+      password: 'test', 
+      name: 'Club Natació Badalona', 
+      logo: 'img/logo/nataciobadalona.jpg' 
+  },
+  { 
+      email: 'betulo@gmail.com', 
+      password: 'test', 
+      name: 'Club Nàutic Bétulo', 
+      logo: 'img/logo/betulo.jpg' 
+  },
+  { 
+      email: 'aexinoxano@gmail.com', 
+      password: 'test', 
+      name: 'A.E. Xino Xano Deltebre', 
+      logo: 'img/logo/aexinoxano.jpg' 
+  },
+  { 
+      email: 'nauticflix@gmail.com', 
+      password: 'test', 
+      name: 'Club Nàutic Flix', 
+      logo: 'img/logo/flix.jpg' 
+  },
+  { 
+      email: 'vecambrills@gmail.com', 
+      password: 'test', 
+      name: "V.E. Vogadors de Cambrills", 
+      logo: 'img/logo/vecambrills.jpg' 
+  },
+  { 
+      email: 'remcambrills@gmail.com', 
+      password: 'test', 
+      name: 'Club de Rem Cambrills', 
+      logo: 'img/logo/remcambrills.jpg' 
+  },
+  { 
+      email: 'remmataro@gmail.com', 
+      password: 'test', 
+      name: 'Club de rem Mataró', 
+      logo: 'img/logo/remmataro.jpg' 
+  },
   ]);
 
   await db.insert(Boat_type).values([
