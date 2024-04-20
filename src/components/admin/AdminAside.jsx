e2b0eee78ee145f9aa7f08ee2c4b934329bdd9f3
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-20 21:40:34 +0200

    Inicio de parte admin

---
+++ src/components/admin/AdminAside.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AdminAside = ({  }) => {
+    return (
+        <aside className="">
+            <ul className="text-white">
+                <li>
+                    <a  href="">Añadir equipos</a>
+                </li>
+            </ul>
+        </aside>
+    );
+}
+
+export default AdminAside;
