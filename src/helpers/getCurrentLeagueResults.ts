f2054144985ff5e027e1035f94231d25cee73a29
Iker Gonzalez Tirado <linkervideogames@gmail.com>
2024-04-29 19:59:35 +0200

    Años y ligas por año en resultado

--- src/helpers/getCurrentLeagueResults.ts
+++ src/helpers/getCurrentLeagueResults.ts
@@ -1,31 +1,27 @@
 import { Competition, Competition_Result, db, eq } from "astro:db";
 import type { TCompetitionResults } from "../types/TCompetitionResults";
-import { getYearFromDate } from "./getYearFromDate";
 import type { TCompetition } from "../types/TCompetition";
 import { League } from "./league";
 import { getTimeInMilliseconds } from "./getTimeInMilliseconds";
 
 export async function getCurrentLeagueResults(year) {
-    let league = new League;
-    
-    const competitions: TCompetition[] = await db.select().from(Competition).where(eq(Competition.year, year))
-    const results: TCompetitionResults[] = await db.select().from(Competition_Result)
+    const league = new League();
+    const competitions: TCompetition[] = await db.select().from(Competition).where(eq(Competition.year, year));
+    const results: TCompetitionResults[] = await db.select().from(Competition_Result);
 
-    competitions.forEach(competition => {
-        
+    for (const competition of competitions) {
+
         const competitionResults = results.filter(result => {
             return result.competition_id === competition.id && result.isLeague && result.isFinal;
         });
         
-        const groupedResults = competitionResults.reduce((acc, obj) => {
-            const { category } = obj;
+        // Agrupar los resultados por categoría y ordenarlos por tiempo
+        const groupedResults = competitionResults.reduce((acc, result) => {
+            const { category } = result;
             
-            if (!acc[category]) {
-                acc[category] = [];
-            }
+            if (!acc[category]) acc[category] = [];
             
-            acc[category].push(obj);
-            
+            acc[category].push(result);
             // Ordenar los resultados por tiempo
             acc[category].sort((a, b) => {
                 const timeA = getTimeInMilliseconds(a.time);
@@ -35,19 +31,20 @@ export async function getCurrentLeagueResults(year) {
             
             return acc;
         }, {});
-          
+        
+        // Asignar puntos a los equipos en función de su posición en cada categoría
         for (const category in groupedResults) {
-            let points = 20;
-            const categoryArray = category.split("")
             if (groupedResults.hasOwnProperty(category)) {
-              const items = groupedResults[category];
-              items.forEach(item => {
-                league.addCategoryResult(competition.boat_type, categoryArray, item.team_id, points)
-                points -= 1
-              });
+                let points = 20;
+                const categoryArray = category.split("");
+                const items = groupedResults[category];
+                items.forEach(item => {
+                    league.addCategoryResult(competition.boat_type, categoryArray, item.team_id, points);
+                    points -= 1;
+                });
             }
-          }
-    });
+        }
+    }
 
     return league;
 }
